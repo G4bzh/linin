bd34bb9090222f8308b9088fa345206c8167b384
Gabriel Viel <gabriel.viel@cailabs.com>
2021-05-15 12:36:44 +0200

    radios onchange handling

--- popup.js
+++ popup.js
@@ -8,21 +8,28 @@ function tmplMessage(fname, isCEO) {
 
     const strCIO = `bonjour ${fname},
 
-    En tant que professionnel de l'informatique du grand ouest comme vous, j'ai pensé qu'il serait pertinent de se connecter pour échanger sur nos aventures et challenges du moment.
+En tant que professionnel de l'informatique du grand ouest comme vous, j'ai pensé qu'il serait pertinent de se connecter pour échanger sur nos aventures et challenges du moment.
 
-    Gabriel
-    Fondateur VeryFrog`
+Gabriel
+Fondateur VeryFrog`
 
     const strCEO = `Bonjour ${fname},
 
-    En tant que dirigeant du grand Ouest comme vous, j'ai pensé qu'il serait pertinent de se connecter afin d'échanger sur nos aventures entrepreneuriales.
+En tant que dirigeant du grand Ouest comme vous, j'ai pensé qu'il serait pertinent de se connecter afin d'échanger sur nos aventures entrepreneuriales.
 
-    Gabriel
-    Fondateur VeryFrog`
+Gabriel
+Fondateur VeryFrog`
 
-    return (isCEO ? strCEO : srtCIO);
-
+    return (isCEO ? strCEO : strCIO);
 }
+
+// Globals
+let lastName = "<LAST_NAME>"
+let firstName  = "<FIRST_NAME>";
+
+// Radios onChange
+let radios = document.querySelectorAll('input[type=radio][name="type"]');
+radios.forEach(radio => radio.addEventListener('change', () => document.getElementById("txtMessage").textContent = tmplMessage(firstName, document.getElementById("rCEO").checked) ));
 
 // Send message to notify popup open event
 console.log("Popup Open. Send message to Background.js")
@@ -34,10 +41,6 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.popupMsg) {
 
         // Prepare for first and last name extraction
-        let txtMessage = document.getElementById("txtMessage");
-        let lblName = document.getElementById("lblName");
-        let lastName = "<LAST_NAME>"
-        let firstName  = "<FIRST_NAME>";
         let reName = request.popupMsg.match(re_name);
 
         // DO we have a match ?
@@ -48,8 +51,8 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
         }
 
         // Print names into popup
-        lblName.textContent = firstName + " " + lastName;
-        txtMessage.textContent = tmplMessage(firstName,true);
+        document.getElementById("lblName").textContent = firstName + " " + lastName;
+        document.getElementById("txtMessage").textContent = tmplMessage(firstName, document.getElementById("rCEO").checked);
         
     }
 
