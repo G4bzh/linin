e40e71a9fe9ee407e17f8cc22647a08d4ce5721a
Gabriel Viel <gabriel.viel@cailabs.com>
2021-05-15 11:55:34 +0200

    strMessage

--- popup.js
+++ popup.js
@@ -1,6 +1,28 @@
 // Constants
- const re_name = /^(?:\(\d{1,}\)\s)?([\w\-]+)\s(.+)\s\|\sLinkedIn.*$/
+const re_name = /^(?:\(\d{1,}\)\s)?([\w\-]+)\s(.+)\s\|\sLinkedIn.*$/
 
+
+// Functions
+
+function tmplMessage(fname, isCEO) {
+
+    const strCIO = `bonjour ${fname},
+
+    En tant que professionnel de l'informatique du grand ouest comme vous, j'ai pensé qu'il serait pertinent de se connecter pour échanger sur nos aventures et challenges du moment.
+
+    Gabriel
+    Fondateur VeryFrog`
+
+    const strCEO = `Bonjour ${fname},
+
+    En tant que dirigeant du grand Ouest comme vous, j'ai pensé qu'il serait pertinent de se connecter afin d'échanger sur nos aventures entrepreneuriales.
+
+    Gabriel
+    Fondateur VeryFrog`
+
+    return (isCEO ? strCEO : srtCIO);
+
+}
 
 // Send message to notify popup open event
 console.log("Popup Open. Send message to Background.js")
@@ -27,7 +49,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 
         // Print names into popup
         lblName.textContent = firstName + " " + lastName;
-        txtMessage.textContent = firstName;
+        txtMessage.textContent = tmplMessage(firstName,true);
         
     }
 
